dc20b08f35d603124ed62cb006264687bd6f454a
Pearstopher <pearstopher@gmail.com>
2023-09-21 11:16:02 -0700

    parsing through dictionary list to reduce it's size

--- frontend/src/Search.jsx
+++ frontend/src/Search.jsx
@@ -1,9 +1,36 @@
-import React, { forwardRef, useEffect, useState, useRef } from 'react'
+import React, {
+    forwardRef,
+    useEffect,
+    useState,
+    useCallback,
+    useRef,
+} from 'react'
+
+function useStateCallback(initialState) {
+    const [state, setState] = useState(initialState)
+    const cbRef = useRef(null) // init mutable ref container for callbacks
+
+    const setStateCallback = useCallback((state, cb) => {
+        cbRef.current = cb // store current, passed callback in ref
+        setState(state)
+    }, []) // keep object reference stable, exactly like `useState`
+
+    useEffect(() => {
+        // cb.current is `null` on initial render,
+        // so we only invoke callback on state *updates*
+        if (cbRef.current) {
+            cbRef.current(state)
+            cbRef.current = null // reset callback after execution
+        }
+    }, [state])
+
+    return [state, setStateCallback]
+}
 
 export const Search = forwardRef(function Search(props, ref) {
-    const [data, setData] = useState(null)
+    const [data, setData] = useStateCallback(null)
     const [error, setError] = useState(null)
-    const [resultMessage, setResultMessage] = useState('')
+    const [resultMessage, setResultMessage] = useState([])
 
     const fetchData = async () => {
         try {
@@ -11,17 +38,50 @@ export const Search = forwardRef(function Search(props, ref) {
                 'https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json'
             )
             const result = await response.json()
-            setData(result)
-            //console.log(JSON.parse(result))
-            setResultMessage(
-                resultMessage +
-                    'Loaded dictionary list... (' +
-                    result.length +
-                    ' items)'
-            )
+            setData(result, () => console.log(data))
+            const newMessage =
+                'Loaded dictionary list... (' +
+                Object.keys(result).length +
+                ' items)'
+            setResultMessage((resultMessage) => [...resultMessage, newMessage])
         } catch (error) {
             setError(error)
         }
+    }
+
+    const removeExtraWords = () => {
+        console.log(data)
+        const goodWords = data.filter((word) => !badWord(word))
+        setData(goodWords)
+        const newMessage =
+            'Optimizing list... (' + Object.keys(goodWords).length + ' items)'
+        setResultMessage((resultMessage) => [...resultMessage, newMessage])
+    }
+    const badWord = (word) => {
+        let bad = false
+
+        // reject any word with that contains a letter thats not on the board
+        const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+        const lettersToRemove = props.inputs // Example list of letters to remove
+        const badLetters = alphabet.filter(
+            (letter) => !lettersToRemove.includes(letter)
+        )
+        if (badLetters.some((letter) => word.includes(letter))) {
+            bad = true
+        }
+
+        // reject any word that's two characters or less
+        if (word.length < 3) {
+            bad = true
+        }
+
+        // reject any word that has any consecutive letters (could preprocess this and make my own dictionary)
+        const letterArray = word.split('')
+        const set = new Set(letterArray)
+        if (set.size !== letterArray.length) {
+            bad = true
+        }
+        return bad
     }
 
     useEffect(() => {
@@ -37,7 +97,10 @@ export const Search = forwardRef(function Search(props, ref) {
 
     return (
         <div>
-            {data && <div>{resultMessage}</div>}
+            {data &&
+                resultMessage.map((message, index) => (
+                    <span key={index}>{message}</span>
+                ))}
             {error && <div>Error: {error.message}</div>}
         </div>
     )
