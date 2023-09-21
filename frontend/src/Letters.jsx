311173f188442ad29af668ce441604b1fb15f969
Pearstopher <pearstopher@gmail.com>
2023-09-20 17:27:00 -0700

    check letters for validity (no missing, no duplicates)

--- frontend/src/Letters.jsx
+++ frontend/src/Letters.jsx
@@ -4,18 +4,40 @@ export const Letters = forwardRef(function Letters(props, ref) {
     const [letters, setLetters] = useState(
         Array('L', 'E', 'T', 'T', 'E', 'R', '-', 'B', 'O', 'X', 'E', 'D')
     )
+    const [valid, setValid] = useState(false)
+
+    function noDuplicates(arr) {
+        const set = new Set(arr)
+        return set.size === arr.length
+    }
+    function noBadCharacters(arr) {
+        return !arr.includes('')
+    }
+
+    useEffect(() => {
+        setValid(noDuplicates(props.inputs) && noBadCharacters(props.inputs))
+    })
 
     return (
-        <div className="letters-container">
-            {props.inputs.map((_, index) => (
-                <span key={index} className={('letter', 'letter-' + index)}>
-                    {props.inputs[index] === '' ? (
-                        <span className={'grey'}>{letters[index]}</span>
-                    ) : (
-                        props.inputs[index]
-                    )}
-                </span>
-            ))}
+        <div className="submit-wrapper">
+            <div className="letters-container">
+                {props.inputs.map((_, index) => (
+                    <span key={index} className={('letter', 'letter-' + index)}>
+                        {props.inputs[index] === '' ? (
+                            <span className={'grey'}>{letters[index]}</span>
+                        ) : (
+                            props.inputs[index]
+                        )}
+                    </span>
+                ))}
+            </div>
+            <div className="valid-container">
+                {valid ? (
+                    <span className="valid">These letters are valid.</span>
+                ) : (
+                    <span className="invlid">These letters are not valid.</span>
+                )}
+            </div>
         </div>
     )
 })
